b6e1d1fc1b7b5ad954897c42fb0580c4866bb226
Soft-Atom <tabproant@gmail.com>
2023-09-01 13:49:55 +0300

    fimally fix

--- exercises/4-4-multithreads/multithreads2.js
+++ exercises/4-4-multithreads/multithreads2.js
@@ -1,19 +1,18 @@
 const getCount = require('./getCount');
 const perfFunc = require('./perfFunc');
+const os = require('node:os');
 
 async function main() {	
 	try {
 		const END = 300_000_003;
-		const CORE_COUNT = 4;
+		const CORE_COUNT = os.cpus().length;
 		const elCount = Math.floor(END / CORE_COUNT); 
-		const arr = (new Array(CORE_COUNT - 1)).fill(1);
-		console.log(
-			(await Promise.all([
-				...arr.map((_, index) => getCount(index * elCount + 1, (index + 1) * elCount)),
-				getCount((CORE_COUNT - 1) * elCount +1, END)
-			]))
-			.reduce((acc,el) => acc += el, 0)
-		);
+		const arr = new Array(CORE_COUNT - 1).fill(1);
+		const res = (await Promise.all([
+			...arr.map((_, index) => getCount(index * elCount + 1, (index + 1) * elCount)),
+			getCount((CORE_COUNT - 1) * elCount +1, END)
+		])).reduce((acc,el) => acc += el, 0);
+		console.log(res);
 	} catch(e) {
 		if (e instanceof Error) console.log(e.message);
 	}
--- exercises/4-5-timer-notifications/timer-notifications.js
+++ exercises/4-5-timer-notifications/timer-notifications.js
@@ -1,14 +1,13 @@
 const MyTimer = require('../4-2-timer/my-timer');
-const notifier = require('node-notifier')
+const notifier = require('node-notifier');
 
 class TimerNotifier extends MyTimer {
 	constructor(notifier, timestring){
 		super(timestring);
 		this._notifier = notifier;
 	}
-	
+
 	_timerCallback() {
-		console.log(this._myTime)
 		this._notifier.notify('Timer');
 	}
 }
